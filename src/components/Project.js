7b6d9245e979618f193c27b9f0564729e9471666
RaneWallin <rane@ranewallin.com>
2019-05-17 20:51:10 -0700

    deleting a project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
+
+const PROJECT_API = "http://localhost:5000/api";
 
 const Project = props => {
-    return (
-        <div className="row">
-            <div className="col s12 m6">
-                <div className="card blue-grey darken-1">
-                    <div className="card-content white-text">
-                        <span className="card-title">{ props.project.name }</span>
-                        <p>{ props.project.description }</p>
-                    </div>
-                    <div className="card-action">
-                        <a href="#">View</a>
-                        <a href="#">Delete</a>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="row">
+      <div className="col s12 m6">
+        <div className="card blue-grey darken-1">
+          <div className="card-content white-text">
+            <span className="card-title">{props.project.name}</span>
+            <p>{props.project.description}</p>
+          </div>
+          <div className="card-action">
+            <a href="#">View</a>
+            <a href="#" onClick={e => handleClick(props.project.id)}>
+              Delete
+            </a>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
+
+const handleClick = id => {
+  axios.delete(`${PROJECT_API}/projects/${id}`);
+};
 
 export default Project;
